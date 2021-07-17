02d0d1c6c0a9b90ad589b87aca2a694fbdbf1bc4
souvikm2002 <souvikm110902@gmail.com>
2021-07-17 12:26:37 +0530

    first commit

--- src/Navber.js
+++ src/Navber.js
@@ -4,6 +4,7 @@ import {Link} from "react-router-dom";
 const Navbar = () => {
     return ( 
         <nav className="Navbar">
+            <h2><img src="https://img.icons8.com/color/48/000000/circled-user-male-skin-type-4--v1.png" className="Icon"/></h2>
             <h1><img src="https://img.icons8.com/color/48/000000/circled-user-male-skin-type-4--v1.png" className="Icon"/>Stephen</h1>
             <div className="Links">
                 <Link to="/Post-App/">Home</Link>
