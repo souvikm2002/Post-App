d5bdc18286485bbafc4912ec9d9395fa778a71b4
souvikm2002 <souvikm110902@gmail.com>
2021-07-17 20:29:56 +0530

    designs

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Home from './My components/Home';
 import Signin from './My components/Signin';
 import Signup from './My components/Signup';
 import { useState } from "react";
+import { useEffect } from 'react';
 import {
   BrowserRouter as Router,
   Switch,
@@ -11,6 +12,15 @@ import {
 } from "react-router-dom";
 
 function App() {
+
+  const [loading,setloading]=useState(false);
+
+  useEffect(() => {
+    setloading(true);
+    setTimeout(() => {
+      setloading(false);
+    }, 1000);
+  }, [])
 
   const [posts,setpost] = useState([
     {sno: 1 ,username: 'Chat-Bot', desc: 'Hey there, lets add a new Post' , like:0 , dislike: 0},
@@ -31,9 +41,13 @@ function App() {
         <Switch>
         <Route exact path="/Post-App/" render={()=>{
             return (<>
+            {loading? (
+              <div className="Loading"></div>
+            ):(
             <div className="content">
              <Home posts={posts} onDelete={onDelete} setpost={setpost}/>
              </div>
+            )}
             </>)
           }}>
           </Route>
